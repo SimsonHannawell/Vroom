898b269243b9310652e87c6595dd4386efdf7875
Namra Mahmood <namramahmood423@gmail.com>
2025-05-21 14:32:01 +0100

    home page complete

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,52 +1,48 @@
 class CarsController < ApplicationController
-
   def index
-
     @cars = Car.all
-
   end
 
   def show
-
     @car = Car.find(params[:id])
-
   end
 
   def new
-
     @car = Car.new
-
   end
 
   def create
-
-    if @car.save?
+    @car = Car.new(car_params)
+    if @car.save
       redirect_to @car
-
     else
       render :new, status: :unprocessable_entity
     end
-
   end
 
   def edit
-
     @car = Car.find(params[:id])
-
   end
 
   def update
     @car = Car.find(params[:id])
+    if @car.update(car_params)
+      redirect_to @car
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path
   end
 
   private
 
   def car_params
-
+    params.require(:car).permit(:make, :model, :year, :fuel, :transmission, :image_url)
   end
-
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 
+puts "Clearing database..."
 
-puts "clearing databse"
 Car.destroy_all
 User.destroy_all
 
-puts "creating new users"
+puts "Creating new users..."
 
 bob = User.create!(
   email: "bob@gmail.com",
@@ -27,9 +18,46 @@ chris = User.create!(
   email: "chris@gmail.com",
   password: "password789"
 )
-puts "created #{User.count} users"
-#cars
-puts "creating new cars"
+steve = User.create!(
+  email: "steve@gmail.com",
+  password: "password123"
+)
+andy = User.create!(
+  email: "andy@gmail.com",
+  password: "password456"
+)
+harry = User.create!(
+  email: "harry@gmail.com",
+  password: "password789"
+)
+alex = User.create!(
+  email: "alex@gmail.com",
+  password: "password123"
+)
+tony = User.create!(
+  email: "tony@gmail.com",
+  password: "password456"
+)
+laura = User.create!(
+  email: "laura@gmail.com",
+  password: "password789"
+)
+beth = User.create!(
+  email: "beth@gmail.com",
+  password: "password123"
+)
+mo = User.create!(
+  email: "mo@gmail.com",
+  password: "password456"
+)
+james = User.create!(
+  email: "james@gmail.com",
+  password: "password789"
+)
+
+puts "Created #{User.count} users"
+
+puts "Creating new cars..."
 
 Car.create!(
   make: "BMW",
@@ -38,8 +66,8 @@ Car.create!(
   fuel: "Diesel",
   transmission: "Auto",
   user: bob
+  image_url: "bmw car.jpg"
 )
-
 Car.create!(
   make: "Volkswagen",
   model: "Beetle",
@@ -48,7 +76,6 @@ Car.create!(
   transmission: "Manual",
   user: alice
 )
-
 Car.create!(
   make: "Mercedes",
   model: "G-wagon",
@@ -57,7 +84,78 @@ Car.create!(
   transmission: "Auto",
   user: chris
 )
+Car.create!(
+  make: "BMW",
+  model: "3 Series",
+  year: 2017,
+  fuel: "Diesel",
+  transmission: "Auto",
+  user: steve
+)
+Car.create!(
+  make: "Volkswagen",
+  model: "Golf",
+  year: 2019,
+  fuel: "Hybrid",
+  transmission: "Manual",
+  user: andy
+)
+Car.create!(
+  make: "Mercedes",
+  model: "A-Class",
+  year: 2020,
+  fuel: "Petrol",
+  transmission: "Auto",
+  user: harry
+)
+Car.create!(
+  make: "BMW",
+  model: "M4",
+  year: 2019,
+  fuel: "Petrol",
+  transmission: "Auto",
+  user: alex
+)
+Car.create!(
+  make: "Volkswagen",
+  model: "Touraeg",
+  year: 2022,
+  fuel: "Diesel",
+  transmission: "Auto",
+  user: tony
+)
+Car.create!(
+  make: "Mercedes",
+  model: "C-class",
+  year: 2023,
+  fuel: "Diesel",
+  transmission: "Auto",
+  user: laura
+)
+Car.create!(
+  make: "Citreon",
+  model: "C4",
+  year: 2015,
+  fuel: "Diesel",
+  transmission: "Manual",
+  user: beth
+)
+Car.create!(
+  make: "Peugeot",
+  model: "208",
+  year: 2018,
+  fuel: "Petrol",
+  transmission: "Auto",
+  user: mo
+)
+Car.create!(
+  make: "Audi",
+  model: "A4",
+  year: 2020,
+  fuel: "Petrol",
+  transmission: "Auto",
+  user: james
+)
 
-puts "created #{Car.count} users"
+puts "Created #{Car.count} cars"
 
-#users
