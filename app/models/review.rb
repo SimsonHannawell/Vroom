3c93c38ddadbab1622c7de0ba80be648cc8b5c69
Ibrahim Ali <ibbyali2@gmail.com>
2025-06-17 13:06:12 +0100

    review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   validates :comment, presence: true
   validates :comment, length: { minimum: 6 }
   validates :rating, presence: true
-  validates :rating, numericality: [0..5]
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
